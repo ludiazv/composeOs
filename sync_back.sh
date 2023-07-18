a0dae174b10f7508767506a1d5954ce0a744057f
ludiazv <ldiaz@atloideas.com>
2023-07-18 11:39:21 +0200

    fix syn_back helper

--- sync_back.sh
+++ sync_back.sh
@@ -3,13 +3,13 @@
 #
 # sshfs root@192.168.50.46:/ mnt
 
-echo "sync back lib"
+echo "sync back lib+env"
 cp -v mnt/lib/composeos/*.sh meta-composeos/recipes-core/composeoslib/files/.
 cp -v mnt/lib/composeos/env meta-composeos/recipes-core/composeoslib/files/.
 echo "sync back etc"
-cp -v mnt/etc/composeos/composeos.conf meta-composeos/recipes-core/composeoslib/files/.
+cp -v mnt/etc/composeos.conf meta-composeos/recipes-core/composeoslib/files/.
 echo "sync boot"
-cp -v mnt/boot/composeos.yml meta-composeos/recipes-core/bootfiles/files/.
+cp -v mnt/boot/composeos.yml meta-composeos/recipes-core/bootfiles/files/cosdbg.yml
 echo "cos utility"
 cp -v mnt/usr/bin/cos meta-composeos/recipes-core/composeoslib/files/cos.sh
 
